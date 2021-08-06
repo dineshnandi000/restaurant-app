8595e46764ca6e3033fa6da7e143eb0526434836
Dinesh Nandi <dineshnandi082@gmail.com>
2021-08-06 17:50:36 +0530

    updated jenkinsh file

--- JenkinsFile.sh
+++ JenkinsFile.sh
@@ -1,31 +1,10 @@
-node() {
-    timeout(time: 2, unit: 'HOURS') {
-        def err = null;
-
-        try {
-            execStage('Clean Workspace', CLEAN_WORKSPACE,{
-                deleteDir()
-            })
-
-            execStage('Checkout scm', SCM_CHECKOUT,{
-                checkout scm
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: '*/master']]
-                ])
-            })
-
-            execStage('Main Build', MAIN_BUILD,{
-                sh "mvn clean install"
-            })
-        }catch(error){
-            println "Error while executing Pipeline for Restaurant API : " + error
-            err=error
-        }finally{
-            if(err){
-                throw err;
+pipeline {
+    agent any
+    stages{
+        stage('Build'){
+            steps{
+                sh 'mvn -B clean verify'
             }
         }
-
     }
 }
